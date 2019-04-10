90707bdd83a6f2b1c4509340b27dd759abfd8e54
zhaozongchang <xingyingsuifeng@gmail.com>
2019-04-10 15:41:31 +0800

    添加删除编辑功能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,29 @@ class PostsController < ApplicationController
    end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+    redirect_to posts_path
+  else
+    render 'eidt'
+   end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end 
 
   private
 
