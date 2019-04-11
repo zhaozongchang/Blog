5bded3c86227cf5f436b2db90ccb1cf0825c8985
zhaozongchang <xingyingsuifeng@gmail.com>
2019-04-11 09:07:27 +0800

    添加权限

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190410015647) do
+ActiveRecord::Schema.define(version: 20190411010240) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
